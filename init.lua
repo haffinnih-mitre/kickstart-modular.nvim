c10d2656b5134742d9b4df344faa05b11e9c5b7e
Habib Affinnih <haffinnih@mitre.org>
2025-05-05 14:33:04 -0400

    Add switch for disabling Lazy when using nvim in VSCode extension

--- init.lua
+++ init.lua
@@ -99,11 +99,16 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+if vim.g.vscode then
+  -- VSCode extension
+else
+  -- ordinary Neovim
+  -- [[ Install `lazy.nvim` plugin manager ]]
+  require 'lazy-bootstrap'
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+  -- [[ Configure and install plugins ]]
+  require 'lazy-plugins'
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
