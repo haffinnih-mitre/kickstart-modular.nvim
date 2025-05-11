3988dcdd0441df36fab733305da39d60d811abb6
Habib Affinnih <haffinnih@gmail.com>
2025-05-11 13:25:48 -0400

    Update catppuccin theme
    light -> dark
    add transparency to play with Alacritty transparency

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -3,6 +3,11 @@ return {
   name = 'catppuccin',
   priority = 1000,
   init = function ()
-    vim.cmd.colorscheme "catppuccin-latte"
+    vim.cmd.colorscheme "catppuccin-macchiato"
+  end,
+  config = function ()
+    require('catppuccin').setup{
+      transparent_background=true
+    }
   end
 }
